af59f55eb8b55b1038ea8f03c88a753c94b05bf0
Milan <milan.3sa@gmail.com>
2022-11-26 18:36:49 -0500

    basic file connections

---
+++ board.h
@@ -0,0 +1,8 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include <string>
+#include <iostream>
+
+class board: public subject{
+    
+}
---
+++ player.h
@@ -0,0 +1,8 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+#include <iostream>
+
+class player: public board {
+    
+}
